189a0d763cde3ec64778e69c2129a8981a77d7cc
Ge Yuwei <yuwei2014@qq.com>
2016-08-13 01:42:51 +0800

    First project HelloWorld
    Second project TestJava
    
    Signed-off-by: Ge Yuwei <yuwei2014@qq.com>

---
+++ TestJava/src/priv/geyuwei/learnjava/testjava/TestJava.java
@@ -0,0 +1,12 @@
+package priv.geyuwei.learnjava.testjava;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Java!");
+
+	}
+
+}
